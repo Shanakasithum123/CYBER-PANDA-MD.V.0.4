7085d95dcd82d294daaf321b24dff5b55497b578
Shanakasithum123 <165706957+Shanakasithum123@users.noreply.github.com>
2024-04-08 18:00:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ global.antitags = true;
 
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = false;                //auto recording by default off.
+global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
-global.author = "SACHITH";                               //Do not change.
+global.Owner = ["+94766763653", "+94766763653"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["94766763653", "+94766763653"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94766763653"];
+global.OwnerName = "Sithumd";
+global.BotName = "sithumd";
+global.packname = "🦋 │𝐂S𝐘I𝐁𝜩𝐑│T𝐏H𝜟𝐍U𝐃𝐀│𝐌M𝐃│𝐕D➂ 🦋";                             //Do not change.
+global.author = "Sithumd";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
